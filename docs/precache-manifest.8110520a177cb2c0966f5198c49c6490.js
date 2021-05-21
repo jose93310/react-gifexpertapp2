c564face1b1229e52affc4143079e32bc6eb227c
Jose Sanabria <JSanabria93310@gmail.com>
2021-05-21 19:37:30 -0400

    docs agregado

---
+++ docs/precache-manifest.8110520a177cb2c0966f5198c49c6490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbbce05afd53a4fee55be4d30b8871f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c5817a89b0fbc9b8338",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5817a89b0fbc9b8338",
+    "url": "/static/js/main.80f9dd85.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
